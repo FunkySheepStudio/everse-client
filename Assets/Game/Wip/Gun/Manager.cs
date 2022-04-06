e2e0aff2800b20c173c0222cc409fcbad7c63ee2
BartKnucle <sjarde@yahoo.fr>
2022-04-06 17:51:13 +0200

    Add guns

---
+++ Assets/Game/Wip/Gun/Manager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Guns
+{
+  public class Manager : MonoBehaviour
+  {
+    public Gun gun;
+  }  
+}
---
+++ Assets/Game/Wip/Gun/Uzi/Uzi.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Guns
+{
+  [CreateAssetMenu(menuName = "Game/Guns/Uzi")]
+  public class Uzi : Gun
+  {
+  }
+}
