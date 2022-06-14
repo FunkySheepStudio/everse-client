d8c54d4b23429f7c325fad8d7d1026d1d36b1b36
BartKnucle <sjarde@yahoo.fr>
2022-06-14 16:27:36 +0200

    Added server managment

---
+++ Assets/Game/Components/Netcode/NetcodeServerManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using Unity.Netcode;
+
+namespace Game.Netcode
+{
+    [RequireComponent(typeof(Unity.Netcode.NetworkManager))]
+    public class NetcodeServerManager : MonoBehaviour
+    {
+        NetworkManager _networkManager;
+
+        private void Awake()
+        {
+            _networkManager = GetComponent<NetworkManager>();
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _networkManager.StartHost();
+        }
+    }
+}
