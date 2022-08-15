b71a7572a169da3f74b58832b2bd7f108fb1b05e
BartKnucle <sjarde@yahoo.fr>
2022-08-15 16:35:25 +0200

    Update world and version

--- Assets/Game/Components/Skybox/DayNight.cs
+++ Assets/Game/Components/Skybox/DayNight.cs
@@ -6,17 +6,19 @@ namespace Game.Environnement
 {
     public class DayNight : MonoBehaviour
     {
+        public float speed = 0.5f;
         public FunkySheep.Types.Float timeOfDay;
         public Material skybox;
         public Light sun;
         public bool simulate;
+        public List<Material> nightLightsMaterials;
 
         // Update is called once per frame
         void Update()
         {
             if (simulate)
             {
-                timeOfDay.value += Time.deltaTime;
+                timeOfDay.value += Time.deltaTime * speed;
             }
 
             timeOfDay.value %= 24;
@@ -29,6 +31,11 @@ namespace Game.Environnement
             sun.transform.rotation = Quaternion.Euler((normalizedTimeOfDay * 360) - 90, 0, 0);
 
             skybox.SetFloat("_DayNight", (Mathf.Cos(normalizedTimeOfDay * 2 * Mathf.PI) + 1) / 2);
+
+            foreach (Material material in nightLightsMaterials)
+            {
+                material.SetFloat("_DayNight", (Mathf.Cos(normalizedTimeOfDay * 2 * Mathf.PI) + 1) / 2);
+            }
         }
 
     }
