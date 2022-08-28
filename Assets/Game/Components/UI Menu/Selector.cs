f172d29fabd34b8562c05345f8465196d045f0ae
BartKnucle <sjarde@yahoo.fr>
2022-08-28 20:09:50 +0200

    Fix circular menu

--- Assets/Game/Components/UI Menu/Selector.cs
+++ Assets/Game/Components/UI Menu/Selector.cs
@@ -17,26 +17,9 @@ namespace Game.UI.CircleMenu
 
     public class Selector : MonoBehaviour, IPointerEnterHandler
     {
-        public List<Button> buttons;
-        public GameObject prefab;
-        List<GameObject> items = new List<GameObject>();
-
-        /*private void Start()
-        {
-            foreach (Button button in buttons)
-            {
-                GameObject buttonGo = GameObject.Instantiate(prefab, transform);
-                buttonGo.GetComponent<UnityEngine.UI.Image>().material.color = button.color;
-                buttonGo.GetComponent<UnityEngine.UI.Image>().sprite = button.icon;
-                items.Add(buttonGo);
-            }
-        }*/
-
         public void OnPointerEnter(PointerEventData eventData)
         {
-            GetComponent<Canvas>().enabled = true;
-            //int index = items.IndexOf(eventData.pointerEnter.gameObject);
-            //GetComponent<UnityEngine.UI.Text>().text = buttons[index].text;
+            //eventData.hovered[0].GetComponent<UnityEngine.UI.Image>().material.SetColor("_Color", Color.black);
         }
     }
 }
